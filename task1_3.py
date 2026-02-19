93a8c02a36ad1ef513638bdb9c54a70dc7e638a8
Alena <a_alzo@mail.ru>
2026-02-19 10:29:42 +0300

    Добавлено задание 1.3 (POST /calculate)

---
+++ task1_3.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+@app.post("/calculate")
+async def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {"result": result}
